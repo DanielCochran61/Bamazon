dfd9b538f2c9fde048c5c94da9afb96a29d0250b
Daniel Cochran <daniel.cochran61@gmail.com>
2019-03-07 19:12:34 -0500

    update all files

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 
+const db = require('./models');
+const seed = require('./seeds/db-seeds');
+
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,8 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 require('./routes/api-routes.js')(app);
 require('./routes/html-routes.js')(app);
 
-const db = require('./models');
-const seed = require('./seeds');
+
 
 db.sequelize.sync().then(function () {
     app.listen(PORT, function () {
